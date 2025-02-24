2eedf6ddd7583a24967300fbd854a4717b1830c9
gautamknambiar <gautamknambiar@gmail.com>
2025-02-23 22:06:24 -0500

    Lowered base speed

--- Traversal/vel_pid.py
+++ Traversal/vel_pid.py
@@ -231,7 +231,7 @@ def main():
             else:
                 desired_direction = np.array([0, 0, 0])
             
-            base_speed = 15.0
+            base_speed = 10.0
             desired_vel_vector = base_speed * desired_direction
 
             current_vel_vector = np.array([
