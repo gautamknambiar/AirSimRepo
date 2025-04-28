c6a811fad29d9b71f10d86b56b447942176c633f
gautamknambiar <gautamknambiar@gmail.com>
2025-04-28 09:33:55 -0400

    added qual.py

--- Traversal/qual.py
+++ Traversal/qual.py
@@ -14,12 +14,12 @@ def init():
     """
     client.confirmConnection()
     print('Connection confirmed')
-    client.simLoadLevel('Soccer_Field_Easy')
+    client.simLoadLevel('Qualifier_Tier_1')
     client.enableApiControl(vehicle_name="drone_1")
     client.arm(vehicle_name="drone_1")
     time.sleep(1)
-    start_position = airsimneurips.Vector3r(-1, -2.0, 1.8)
-    start_rotation = airsimneurips.Quaternionr(0, 0, 0, 4.71)
+    start_position = airsimneurips.Vector3r(1.3731292486190796, 81.43741607666016, -43.8)
+    start_rotation = airsimneurips.Quaternionr(0, 0, -0.17364919185638428, 0.984807550907135)
     new_pose = airsimneurips.Pose(start_position, start_rotation)
     client.simSetVehiclePose(new_pose, ignore_collison=True)
 
